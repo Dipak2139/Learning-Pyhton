a6c06827d676e2e5da8551131fcb625c3a0a735d
Dipak2139 <dipakdas7478@gmail.com>
2024-09-20 22:31:48 +0530

    todays work

--- stirngs.py
+++ stirngs.py
@@ -18,3 +18,15 @@ name4 = "rahul"
 #to just print 'ahu'
 updated_name4 = name4[1:4]
 print(updated_name4)
+updated1_name = name[:4]
+print(updated1_name)
+updated2_name = name[1:]
+print(updated2_name)
+
+
+
+#Negative Slicing
+name5 = "rajesh"
+updated_name5 = name5[-4: -1]
+print(updated_name5)
+# print(name5)
---
+++ strings2.py
@@ -0,0 +1,19 @@
+# Skipping technique
+num = "0123456789"
+update1= num[1 : 6 : 2]
+print(update1)
+
+name = "deepusharma"
+updated2 = name[: 7: 3]
+print(updated2)
+
+# Some String functions:
+
+name1 = "dipak"
+print("String functions:")
+print(len(name1))
+# print(upper(name1))
+print(name1.upper())
+
+str = "I am a good boy"
+print(str.replace("good","bad"))
