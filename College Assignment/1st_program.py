3804332549164f9ab186e83a7fda8a5e8daca6dc
Dipak2139 <dipakdas7478@gmail.com>
2024-11-05 12:14:04 +0530

    College Assignment

---
+++ College Assignment/1st_program.py
@@ -0,0 +1,3 @@
+#Take an input from the user, save this into a variable and print the value to the console
+user = input("Enter your name: ")
+print(user)
---
+++ College Assignment/2nd_program.py
@@ -0,0 +1,9 @@
+#Read the attendance percentage from a student and if the attendance is more than 70% print
+#allowed, otherwise print debarred.
+
+attendance = int(input("Enter you Attendance percentage: "))
+
+if(attendance>=75):
+    print("Allowed")
+else:
+    print("Debarred")
